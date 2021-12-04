19d669a4dc6139dee06769db84dd75da5d0a94bb
sami-p <sami.peterson14@gmail.com>
2021-12-03 21:43:04 -0600

    Add task factories

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
-    task_name { "MyString" }
-    status { false }
-    activity { nil }
+    task_name { Faker::Verb.base }
+    completed { Faker::Boolean.boolean }
+    activity { 1 }
   end
 end
