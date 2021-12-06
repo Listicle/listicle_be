d52add1f8cc95955ae219fd51a89f53cdb5bc96e
sami-p <sami.peterson14@gmail.com>
2021-12-06 12:25:08 -0600

    Add Destroy to project mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_project, mutation: Mutations::CreateProject
+    field :destroy_project, mutation: Mutations::DestroyProject
   end
 end
