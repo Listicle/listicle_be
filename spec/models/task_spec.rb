cfa2a06d01b0ca2c1cb72254b4293a20d683969a
kevn7n <kevn_nguyen@yahoo.com>
2021-12-06 13:52:16 -0700

    Fix spell error

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe 'Relationships' do
-    it { should belong_to(:acitivity) }
+    it { should belong_to(:activity) }
   end
 
   describe 'Validations' do
