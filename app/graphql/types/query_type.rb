ebea4920aa6a0d2e664f687dc607eb9478c6cff1
sami-p <sami.peterson14@gmail.com>
2021-12-06 11:52:14 -0600

    Add project, activity, and task query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -21,9 +21,46 @@ module Types
       argument :id, ID, required: true
     end
 
-    def user(id:) #may not need colon 
+    def user(id:) #may not need colon
       User.find(id)
     end
 
+    field :projects, [Types::ProjectType], null: false
+
+    def projects
+      Project.all
+    end
+
+    field :project, Types::ProjectType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def project(id:)
+      Project.find(id)
+    end
+
+    field :activities, [Types::ActivityType], null: false
+
+    def activity
+      Activity.all
+    end
+
+    field :activity, Types::ActivityType, null: false do
+      argument :id, ID, required: true
+    end
+
+    field :tasks, [Types::TaskType], null: false
+
+    def tasks
+      Task.all
+    end
+
+    field :task, Types::TaskType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def task(id:)
+      Task.find(id)
+    end
   end
 end
