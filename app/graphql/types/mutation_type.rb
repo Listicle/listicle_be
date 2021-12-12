6ff541a19b7f41b71765fcd24c71b644023d9d87
sami-p <sami.peterson14@gmail.com>
2021-12-12 15:45:28 -0600

    Add create and destroy task mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_project, mutation: Mutations::CreateProject
     field :destroy_project, mutation: Mutations::DestroyProject
+    field :create_task, mutation: Mutations::CreateTask
+    field :destroy_task, mutation: Mutations::DestroyTask
   end
 end
