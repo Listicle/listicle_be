b85b0e153211ce57def8b26e540c55bfec14f266
sami-p <sami.peterson14@gmail.com>
2021-12-12 12:55:53 -0600

    Build create task mutation

---
+++ app/graphql/mutations/create_task.rb
@@ -0,0 +1,23 @@
+class Mutations::CreateTask < Mutations::BaseMutation
+  argument :task_name, String, required: true
+  argument :completed, Boolean, required: true
+  argument :activity_id, Integer, required: true
+
+  field :task, Types::TaskType, null: false
+  field :errors, [String], null: false
+
+  def resolve(task_name:, activity_id:)
+    task = Task.new(task_name: task_name, activity_id: activity_id)
+        if task.save
+      {
+        task: task,
+        errors: []
+      }
+    else
+      {
+        task: nil,
+        errors: task.errors.full_messages
+      }
+    end
+  end
+end
