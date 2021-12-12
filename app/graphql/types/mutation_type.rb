adcb347e75b89985f2a8fe6b74fb5f4a3e18142b
Erin Quinn <equinn125@gmail.com>
2021-12-12 13:29:44 -0700

    add gql enum file for status and get update method working so far

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_project, mutation: Mutations::CreateProject
     field :destroy_project, mutation: Mutations::DestroyProject
+    field :update_activity, mutation: Mutations::UpdateActivity
   end
 end
