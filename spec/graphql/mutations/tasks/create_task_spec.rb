115c033e5176dee7c6bfe792fc71d93fdcb85e7e
sami-p <sami.peterson14@gmail.com>
2021-12-13 08:47:21 -0600

    Add testing for create and destroy task mutations

---
+++ spec/graphql/mutations/tasks/create_task_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+module Mutations
+  module Tasks
+    RSpec.describe CreateTask,type: :request do
+      describe '.resolve' do
+        it "creates a new task" do
+          user = create(:user)
+          project = create(:project, user_id: user.id)
+          activity = create(:activity, project_id: project.id)
+
+          post '/graphql', params: { query: query(activity_id: activity.id)}
+
+          json = JSON.parse(response.body)
+          data = json['data']['createTask']
+
+          expect(Task.count).to eq(1)
+        end
+
+        it 'returns a task' do
+          user = create(:user)
+          project = create(:project, user_id: user.id)
+          activity = create(:activity, project_id: project.id)
+
+          post '/graphql', params: { query: query(activity_id: activity.id)}
+
+          json = JSON.parse(response.body)
+          data = json['data']['createTask']['task']
+
+          expect(data['id']).to be_present
+          expect(data['id']).to be_a String
+          expect(data['taskName']).to be_a String
+          expect(data['taskName']).to eq("New boot goofin")
+          # expect(data['completed']).to be_a Boolean
+          expect(data['completed']).to eq(true)
+          expect(data['activityId']).to be_a Integer
+          expect(data['activityId']).to eq(activity.id)
+        end
+      end
+
+      def query(activity_id:)
+        <<~GQL
+          mutation {
+           createTask(input: {
+             completed: true
+             taskName: "New boot goofin"
+             activityId: #{activity_id}
+           }) {
+             task {
+               id
+               taskName
+               completed
+               activityId
+             }
+             errors
+           }
+          }
+        GQL
+      end
+    end
+  end
+end
