1b69fea8254bcf25f9241e6e47431e040dd979db
Erin Quinn <equinn125@gmail.com>
2021-12-13 20:54:24 -0700

    inital create method working

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :create_project, mutation: Mutations::CreateProject
     field :destroy_project, mutation: Mutations::DestroyProject
     field :update_activity, mutation: Mutations::UpdateActivity
+    field :create_activity, mutation: Mutations::CreateActivity
   end
 end
